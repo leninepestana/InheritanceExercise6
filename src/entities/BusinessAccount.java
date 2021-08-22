ff57fe0e68cf89857e881eb8214d9d464d2cb399
leninepestana <leninepestana@gmail.com>
2021-08-22 10:35:57 +0100

    Staring project

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class BusinessAccount extends Account {
+	private  Double loanLimit;
+
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(balance <= loanLimit) {
+			balance += amount - 10.0;
+		}
+	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+	}
+}
