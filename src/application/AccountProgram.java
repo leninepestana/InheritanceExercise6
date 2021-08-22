49ec6d93be2ce28345251e4fb4949f035f919e94
leninepestana <leninepestana@gmail.com>
2021-08-22 18:15:19 +0100

    Creating Account List

--- src/application/AccountProgram.java
+++ src/application/AccountProgram.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -8,11 +12,30 @@ public class AccountProgram {
 
 	public static void main(String[] args) {
 		
-		/* Class Account cannot be instantiated because it was created as abstract */
+		Locale.setDefault(Locale.US);
 		
-		//Account accOne = new Account(3459, "Martin", 1000.00);
-		Account accTwo = new BusinessAccount(4891, "Daniel", 1000.00, 300.00);
-		Account accThree = new SavingsAccount(6749, "Marta", 1000.00, 1.01);
+		List<Account> account = new ArrayList<>();
+		
+		account.add(new SavingsAccount(3785, "Martin", 1000.00, 0.01));
+		account.add(new BusinessAccount(4982, "Daniel", 1000.00, 300.00));
+		account.add(new SavingsAccount(7840, "Laura", 700.00, 0.01));
+		account.add(new BusinessAccount(4890, "Maria", 700.00, 400.00));
+		
+		double sum = 0.0;
+		for (Account a : account) {
+			sum += a.getBalance();
+		}
+
+		System.out.println("Total balance: " + sum);
+		System.out.println();
+		
+		for(Account a : account) {
+			a.deposit(10.00);
+		}
+		
+		for(Account a : account) {
+			System.out.printf("Updated accont: %d, Balance: %.2f %n", a.getNumber(), a.getBalance());
+		}
 		
 	}
 
